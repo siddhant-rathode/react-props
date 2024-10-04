b78035abdc51e7c161a6aad732c49a34d3bbf055
Siddhant Rathode <siddhantrathode@gmail.com>
2024-10-04 23:45:39 +0530

    normal

--- src/App.jsx
+++ src/App.jsx
@@ -10,3 +10,46 @@ const App = () => {
 }
 
 export default App
+
+
+
+
+
+
+
+
+
+
+// import Card from './component/Card.jsx'
+
+// const App = () => {
+
+//   const user = [
+//     {
+//       name: "John Doe",
+//       email: "john@gmail.com",
+//       age: 20,
+//     },
+//     {
+//       name: "huihui",
+//       email: "huihui@gmail.com",
+//       age: 30,
+//     },
+//     {
+//       name: "sidd",
+//       email: "sidd@gmail.com",
+//       age: 25,
+//     }
+//   ]
+
+//   return (
+//     <div>
+//       {user.map((user,index) => (
+//         <Card key={index} name={user.name} 
+//         email={user.email} age={user.age} />
+//       ))}
+//     </div>
+//   )
+// }
+
+// export default App
